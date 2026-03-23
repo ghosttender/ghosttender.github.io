92612a2445b1fae2fcd0e015e58dc7b75ea81018
ghosttender <bethoven1999x@gmail.com>
2026-03-23 14:48:26 +0800

    Site updated: 2026-03-23 14:48:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/22/hello-world/","2026/03/23/Linux基础命令/"];function toRandomPost(){
+var posts=["2026/03/23/Linux基础命令/","2026/03/22/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
