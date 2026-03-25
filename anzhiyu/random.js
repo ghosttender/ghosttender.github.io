6320fd6fc1e4ab5d1243d9e6425ce0b79185b96e
ghosttender <bethoven1999x@gmail.com>
2026-03-25 15:09:35 +0800

    Site updated: 2026-03-25 15:09:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/23/Linux进阶命令/","2026/03/23/Linux下载安装+进程+网络命令/","2026/03/22/hello-world/","2026/03/23/Linux基础命令/","2026/03/25/域渗透-白银票据伪造/"];function toRandomPost(){
+var posts=["2026/03/23/Linux基础命令/","2026/03/23/Linux进阶命令/","2026/03/23/Linux下载安装+进程+网络命令/","2026/03/22/hello-world/","2026/03/25/域渗透-白银票据伪造/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
